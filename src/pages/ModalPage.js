9e6bf38a600c723f1134d52f2efc0027b14d7641
Oleksandr Pavlii <pavliy.av@gmail.com>
2024-08-10 18:42:36 +0200

    Init Modal component

---
+++ src/pages/ModalPage.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Button from '../components/Button';
+import Modal from '../components/Modal';
+
+function ModalPage() {
+	const [showModal, setShowModal] = useState(false);
+
+	const handleClick = () => {
+		setShowModal(true);
+	};
+
+	const handleClose = () => {
+		setShowModal(false);
+	};
+
+	return (
+		<div>
+			<Button primary onClick={handleClick}>
+				Open Modal
+			</Button>
+
+			{showModal && <Modal onClose={handleClose} />}
+		</div>
+	);
+}
+
+export default ModalPage;
